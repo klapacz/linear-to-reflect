7d8d98f504ec2b07a32ef5520e8a8e0f203b30d6
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2023-12-01 14:15:50 +0100

    chore: parse data

--- hello.ts
+++ hello.ts
@@ -1,5 +1,6 @@
 import { Context, Hono } from "https://deno.land/x/hono@v3.10.2/mod.ts";
 import { hmac } from "https://deno.land/x/hmac@v2.0.1/mod.ts";
+import { z } from "https://deno.land/x/zod/mod.ts";
 
 const app = new Hono();
 
@@ -7,6 +8,25 @@ const WEBHOOK_SECRET = Deno.env.get("LINEAR_WEBHOOK_SECRET");
 if (!WEBHOOK_SECRET) {
   throw new Error("webhook secret must be provided");
 }
+
+const baseLinearSchema = z.object({
+  action: z.string(),
+});
+
+const createActionLinearSchema = z.object({
+  action: z.literal("create"),
+  data: z.object({
+    title: z.string(),
+    identifier: z.string(),
+    url: z.string().url(),
+    assignee: z
+      .object({
+        name: z.string(),
+      })
+      .optional(),
+    description: z.string().optional(),
+  }),
+});
 
 app.post("/", async (c: Context) => {
   const rawBody = await c.req.arrayBuffer();
@@ -25,13 +45,32 @@ app.post("/", async (c: Context) => {
     return;
   }
 
-  const payload = JSON.parse(new TextDecoder().decode(rawBody));
-  console.log(payload);
+  const okResponse = c.json({ status: "Ok." }, 200);
+
+  const rawPayload = JSON.parse(new TextDecoder().decode(rawBody));
+  const parsedBasePayload = baseLinearSchema.safeParse(rawPayload);
+
+  if (!parsedBasePayload.success) {
+    console.log("No action specified.", parsedBasePayload.error);
+    return okResponse;
+  }
+  if (parsedBasePayload.data.action !== "create") {
+    return okResponse;
+  }
+
+  const parsedCreateActionPayload =
+    createActionLinearSchema.safeParse(rawPayload);
+  if (!parsedCreateActionPayload.success) {
+    console.log("Wrong action payload", parsedCreateActionPayload.error);
+    return okResponse;
+  }
+
+  console.log({ data: parsedCreateActionPayload.data });
 
   // Do something neat with the data received!
 
   // Finally, respond with a HTTP 200 to signal all good
-  return c.json({ status: "Ok." }, 200);
+  return okResponse;
 });
 
 Deno.serve(app.fetch);
