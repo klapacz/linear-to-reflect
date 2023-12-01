fc87153aa14197d1e3dcb53da736a60f3fb412ac
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2023-12-01 14:27:11 +0100

    chore: send ticket to reflect

--- hello.ts
+++ hello.ts
@@ -1,11 +1,13 @@
 import { Context, Hono } from "https://deno.land/x/hono@v3.10.2/mod.ts";
 import { hmac } from "https://deno.land/x/hmac@v2.0.1/mod.ts";
-import { z } from "https://deno.land/x/zod/mod.ts";
+import { z } from "https://deno.land/x/zod@v3.21.4/mod.ts";
 
 const app = new Hono();
 
 const WEBHOOK_SECRET = Deno.env.get("LINEAR_WEBHOOK_SECRET");
-if (!WEBHOOK_SECRET) {
+const REFLECT_ACCESS_TOKEN = Deno.env.get("REFLECT_ACCESS_TOKEN");
+const REFLECT_GRAPH_ID = Deno.env.get("REFLECT_GRAPH_ID");
+if (!WEBHOOK_SECRET || !REFLECT_ACCESS_TOKEN || !REFLECT_GRAPH_ID) {
   throw new Error("webhook secret must be provided");
 }
 
@@ -64,8 +66,34 @@ app.post("/", async (c: Context) => {
     console.log("Wrong action payload", parsedCreateActionPayload.error);
     return okResponse;
   }
+  const {
+    data: {
+      data: { identifier, url, title, assignee, description },
+    },
+  } = parsedCreateActionPayload;
 
-  console.log({ data: parsedCreateActionPayload.data });
+  const noteSubject = `${identifier} ${title}`;
+  const noteAssignee = assignee ? `- Assignee: [[${assignee.name}]]` : ``;
+  const noteDescription = description ? `- Description: ${description}` : ``;
+
+  const noteContent = `
+  - URL: ${url}
+  ${noteDescription}
+  ${noteAssignee}
+  `;
+
+  await fetch(`https://reflect.app/api/graphs/${REFLECT_GRAPH_ID}/notes`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${REFLECT_ACCESS_TOKEN}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      subject: noteSubject,
+      content_markdown: noteContent,
+      pinned: false,
+    }),
+  }).catch((error) => console.error("Error:", error));
 
   // Do something neat with the data received!
 
